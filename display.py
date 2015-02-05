d104950039addef85a765debfd152b73ef51719f
Timothée Rebours <t.rebours@gmail.com>
2015-02-05 02:33:17 +0100

    minor

--- display.py
+++ display.py
@@ -1,14 +1,16 @@
 from __future__ import unicode_literals
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from display_LCD_dummy import displayLCDddummy
-from display_LCDd_2x40 import displayLCDd2x40
+
+
 
 
 class DisplayChooser():
 
     def __init__(self, CONF):
         if CONF.lcd['type'] == '2x40':
+            from display_LCDd_2x40 import displayLCDd2x40
             self.display = displayLCDd2x40(CONF)
         else:
+            from display_LCD_dummy import displayLCDddummy
             self.display = displayLCDddummy()
