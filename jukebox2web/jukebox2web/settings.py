5af9c697f81e4e00dcf75e73c2f6504c2d9d878c
Timothée Rebours <t.rebours@gmail.com>
2015-02-08 04:41:10 +0100

    minor

--- jukebox2web/jukebox2web/settings.py
+++ jukebox2web/jukebox2web/settings.py
@@ -60,7 +60,7 @@ WSGI_APPLICATION = 'jukebox2web.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../db.sqlite3'),
     }
 }
 
