113795bf9a97025d5ec7abad10b063b483344f29
Timothée Rebours <t.rebours@gmail.com>
2015-02-11 17:19:31 +0100

    migrations

---
+++ jukebox2web/library_manager/migrations/0006_auto_20150211_1518.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('library_manager', '0005_auto_20150211_1106'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='music',
+            name='file_field',
+            field=models.FileField(upload_to=b'Library'),
+            preserve_default=True,
+        ),
+    ]
