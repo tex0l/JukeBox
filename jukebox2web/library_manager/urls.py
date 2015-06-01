655d49cbbe72b9464b2d878b238febcf32f821cf
Timothée Rebours <t.rebours@gmail.com>
2015-06-01 11:51:50 +0200

    remove first upload implem

--- jukebox2web/library_manager/urls.py
+++ jukebox2web/library_manager/urls.py
@@ -10,7 +10,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'jukebox2web.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^upload', Upload.as_view(), name='Upload'),
     url(r'^playlist_manager', PlaylistManager.as_view(), name='PlaylistManager'),
     url(r'^ajax/library', Library.as_view(), name='library'),
     url(r'^ajax/upload', AjaxUpload.as_view(), name='library'),
--- jukebox2web/library_manager/views.py
+++ jukebox2web/library_manager/views.py
@@ -10,34 +10,6 @@ from models import Music
 import logging
 
 
-class Upload(View):
-
-
-    def get(self, request):
-        form = MusicForm() # A empty, unbound form
-        musics = Music.objects.all()
-        for music in musics:
-            print music.file_field.name
-        # Render list page with the documents and the form
-        return render_to_response(
-            'upload.html',
-            {'musics': musics, 'form': form},
-            context_instance=RequestContext(request)
-        )
-
-
-    def post(self, request):
-        form = MusicForm(request.POST, request.FILES)
-        if form.is_valid():
-            new_music = Music(file_field=request.FILES['file_field'])
-            new_music.save()
-            new_music.clean()
-            # Redirect to the document list after POST
-            return HttpResponseRedirect('upload')
-        else:
-            return self.get(request)
-
-
 class PlaylistManager(View):
 
     def get(self, request):
