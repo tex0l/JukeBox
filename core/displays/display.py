ce67e7229ccc6e84376a2822204e9396ad016824
Timothée Rebours <t.rebours@gmail.com>
2016-02-11 16:58:38 +0100

    import mistake fixed in core/displays/display.py

--- core/displays/display.py
+++ core/displays/display.py
@@ -12,13 +12,13 @@ class DisplayChooser():
     def __init__(self, loaded_config):
         if loaded_config.lcd['type'] == '2x40':
             try:
-                from lib.displays.display_LCDd_2x40 import DisplayLCDd2x40
+                from core.displays.display_LCDd_2x40 import DisplayLCDd2x40
                 logging.debug("Imported display_LCDd_2x40 library, initializing")
                 self.display = DisplayLCDd2x40(loaded_config)
                 return
             except:
                 logging.warning("2x40 display cannot be initialized, switching to dummy")
 
-        from lib.displays.display_dummy import DisplayDummy
+        from core.displays.display_dummy import DisplayDummy
         logging.debug("Imported DisplayDummy library, initializing")
         self.display = DisplayDummy()
