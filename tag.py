a8e8f61150a226f8b9ec87ec7375f36afb12eb44
victor <victorng@hotmail.fr>
2016-02-22 17:14:06 +0100

    tag.py

---
+++ tag.py
@@ -0,0 +1,37 @@
+
+import json
+from subprocess import call
+# read json file
+with open('tag.json') as data_file:
+    data = json.load(data_file)
+
+#print(data["A"]["1"]["path"])
+
+# read tex file
+f = open("latex/tag.tex", "r")
+contents = f.readlines()
+f.close()
+
+index = 23
+b = True
+# letters = ['A', 'B', 'C', 'D']
+# numbers = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20']
+#
+# for letter in letters:
+#     for number in numbers:
+#         if(b):
+#             line = "\\boite{\content{%s}{%s}{%s}}" % (letter+number,data[letter][number]["title"],data[letter][number]["artist"])
+#             b = False
+#
+#         else:
+#             line += "{\content{%s}{%s}{%s}}\n" % (letter+number,data[letter][number]["title"],data[letter][number]["artist"])
+#             b= True
+#             contents.insert(index, line)
+#             index += 1
+#
+# f = open("latex/tag.tex", "w")
+# f.writelines(contents)
+# f.close()
+
+# generate pdf file in latex directory: latex/tag.pdf
+call(["pdflatex","-output-directory", "latex/","latex/tag.tex"])
